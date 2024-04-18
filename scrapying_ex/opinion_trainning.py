1bb179efb3848182bb151f6f66f523ab2f2dcb50
eycho <chw3872@gmail.com>
2024-04-18 17:14:22 +0900

    matplotlib

--- scrapying_ex/opinion_trainning.py
+++ scrapying_ex/opinion_trainning.py
@@ -1,6 +1,13 @@
+import re
+import urllib.request
+import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt # type: ignore
+plt.style.use('seaborn-white')
+
 import warnings
 warnings.filterwarnings(action='ignore')
+
 
 from konlpy.tag import Okt
 
@@ -15,7 +22,6 @@ train_df = train_df[train_df['리뷰'].notnull()]
 # for i in range(0, len(train_df)) :
 #     print(train_df['리뷰'][i])
 
-import re
 # 불가능한 문자 제거
 # ‘ㄱ ~‘힣’까지의 문자를 제외한 나머지는 공백으로 치환, 영문: a-z| A-Z
 train_df['리뷰'] = train_df['리뷰'].apply(lambda x : re.sub(r'[^ ㄱ-ㅣ가-힣]+', " ", x))
@@ -28,6 +34,3 @@ for i in range(0, len(review)) :
 # Train용 데이터셋과 Test용 데이터 셋 분리
 # 1. 예측력을 높이기 위해 수집된 데이터를 학습용과 테스트 용으로 분리하여 진행
 # 2. 보통 20~30%를 테스트용으로 분리해 두고 테스트
-from sklearn.model_selection import train_test_split
-train_x, test_x = train_test_split(review, test_size=0.2, random_state=0)
-print(len(train_x), len(test_x))
